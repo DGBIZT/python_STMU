1018bb146fd4ad5705246f2139e64fbbe7ba44fc
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-02-25 10:21:13 +0300

    выводит и имя и номер

--- src/widget.py
+++ src/widget.py
@@ -3,10 +3,9 @@ from masks import get_mask_account, get_mask_card_number
 def mask_account_card(card: str) -> str:
     card_list = card.split(" ")
     if len(card_list[-1]) == 16:
-        masks_card = get_mask_card_number(card_list[-1])
+        masks_card = f"{" ".join(card_list[:-1])} {get_mask_card_number(card_list[-1])}"
     else:
-        masks_card = get_mask_account(card_list[-1])
+        masks_card = f"{" ".join(card_list[:-1])} {get_mask_account(card_list[-1])}"
 
     return masks_card
-
 
