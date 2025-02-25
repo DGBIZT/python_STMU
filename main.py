39391925e9d5b68ebb41648473dbe6d022f9eddb
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-02-25 12:03:27 +0300

    Добавил пример работы функции get_date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card
+from src.widget import get_date, mask_account_card
 
 # Маскировка номера банковской карты
 
@@ -13,3 +13,8 @@ print(account_number)
 # Функция которая умеет обрабатывать информацию как о картах, так и о счетах
 mask_ac_card = mask_account_card("Счет 73654108430135874305")
 print(mask_ac_card)
+
+# Функция которая принимает на вход строку с датой в форме "2024-03-11T02:26:18.671407"
+# и возвращает строку с датой в формате "ДД.ММ.ГГГГ"
+data_new = get_date("2024-03-11T02:26:18.671407")
+print(data_new)
