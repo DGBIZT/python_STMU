0d1091a728c10626b17d0b7919fe46055c51b195
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-03-03 11:59:18 +0300

    Создал две функции фильтрующие русские и английские имена

--- nametxt.py
+++ nametxt.py
@@ -1,3 +1,5 @@
+import re
+
 def correct_file_name(name_file: str) -> list:
     new_name_file = list()
     with open('data/' + name_file, "r", encoding= 'utf-8') as file_list:
@@ -13,8 +15,29 @@ def correct_file_name(name_file: str) -> list:
 
         return new_name_file
 
+def filter_russian_names(name_item: str) -> bool:
+    return bool(re.search("[а-яА-Я]", name_item))
+
+def russian_names(list_file: list) -> list:
+    new_rus_list = list()
+    for name_item in list_file:
+        if filter_russian_names(name_item):
+            new_rus_list.append(name_item)
+    return new_rus_list
+
+def english_names(list_file: list) -> list:
+    new_english_list = list()
+    for name_item in list_file:
+        if not filter_russian_names(name_item):
+            new_english_list.append(name_item)
+    return new_english_list
+
+
 
 file = (correct_file_name("names.txt"))
 
 for i in file:
     print(i)
+
+print(russian_names(file))
+print(english_names(file))
